4f1758d23ac1d7b562d22f4c9938d1a8025b6674
OscarCastellanos01 <oscareliascastellanoscordero@gmail.com>
2024-08-17 16:32:53 -0600

    Update favicon url

--- app/views/layouts/head.php
+++ app/views/layouts/head.php
@@ -7,7 +7,7 @@
         <meta name="author" content="" />
         <title><?= $datos['title'] ?></title>
         <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="<?php echo ASSET; ?>/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
